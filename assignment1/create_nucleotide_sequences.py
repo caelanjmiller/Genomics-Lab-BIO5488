268c68ece89b9d8973efc758c824307152789f10
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-01-19 02:28:32 -0600

    Added skeleton of logic flow for sequence generation

--- assignment1/create_nucleotide_sequences.py
+++ assignment1/create_nucleotide_sequences.py
@@ -7,17 +7,23 @@ G_FREQ: float = argv[4]
 T_FREQ: float = argv[5]
 FASTA_OUTPUT: str = argv[6]
 
-NUCLEOTIDE_FREQ: dict = {'A': A_FREQ, 'T': T_FREQ, 'C': C_FREQ, 'G': G_FREQ}
+NUCLEOTIDE_FREQ: dict = {'A': float(A_FREQ), 'T': float(T_FREQ), 'C': float(C_FREQ), 'G': float(G_FREQ)}
 
 def create_novel_dna_sequence(length: int, frequencies: dict) -> dict:
     """ 
     Create a novel DNA sequence that is of a provided length & 
     similar to provided nucleotide frequencies (within 2 decimal places)
     """
-    generated_dna_sequence: str = ''
-    novel_seq: dict = {}
-    novel_seq['GENERATED_SEQUENCE'] = generated_dna_sequence
-    return novel_seq
+    total_nucleotide_frequency = sum(frequencies.values())
+    IDEAL_NUCLEOTIDE_FREQ: float = float(1.0)
+    if total_nucleotide_frequency > IDEAL_NUCLEOTIDE_FREQ or total_nucleotide_frequency < IDEAL_NUCLEOTIDE_FREQ:
+        raise Exception('Provided nucleotide frequencies do not sum to 1')
+    else:
+        # generated_dna_sequence: str = ''
+        # novel_seq: dict = {}
+        # novel_seq['GENERATED_SEQUENCE'] = generated_dna_sequence
+        # return novel_seq
+        return
 
 def sequence_to_FASTA (novel_seq: dict, FASTA_OUTPUT: str):
     """
