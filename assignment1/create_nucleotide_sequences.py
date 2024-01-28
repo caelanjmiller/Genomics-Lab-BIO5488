8b7de315bcf7c5b7b5f54b9afb5fb82bb864b796
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-01-27 23:06:31 -0600

    Ruff formatting

--- assignment1/create_nucleotide_sequences.py
+++ assignment1/create_nucleotide_sequences.py
@@ -9,31 +9,40 @@ G_FREQ: float = float(argv[4])
 T_FREQ: float = float(argv[5])
 FASTA_OUTPUT: str = argv[6]
 
-NUCLEOTIDE_FREQ: dict = {'A': A_FREQ, 'C': C_FREQ, 'G': G_FREQ, 'T': T_FREQ}
+NUCLEOTIDE_FREQ: dict = {"A": A_FREQ, "C": C_FREQ, "G": G_FREQ, "T": T_FREQ}
+
 
 def create_novel_dna_sequence(length: int, frequencies: dict) -> dict:
-    """ 
-    Create a novel DNA sequence that is of a provided length & 
+    """
+    Create a novel DNA sequence that is of a provided length &
     similar to provided nucleotide frequencies (within 2 decimal places)
     """
     total_nucleotide_frequency = sum(frequencies.values())
     IDEAL_NUCLEOTIDE_FREQ: float = float(1.0)
-    if total_nucleotide_frequency > IDEAL_NUCLEOTIDE_FREQ or total_nucleotide_frequency < IDEAL_NUCLEOTIDE_FREQ:
-        raise Exception('Provided nucleotide frequencies do not sum to 1')
+    if (
+        total_nucleotide_frequency > IDEAL_NUCLEOTIDE_FREQ
+        or total_nucleotide_frequency < IDEAL_NUCLEOTIDE_FREQ
+    ):
+        raise Exception("Provided nucleotide frequencies do not sum to 1")
     else:
         # Creates a DNA sequence string of a provided length that from the four choices are weighted (to be chosen at random) by the provided frequencies
-        generated_dna_sequence: str = "".join(random.choices(population=['A', 'C', 'G', 'T'], weights=frequencies.values(), k=length))
+        generated_dna_sequence: str = "".join(
+            random.choices(
+                population=["A", "C", "G", "T"], weights=frequencies.values(), k=length
+            )
+        )
         novel_seq: dict = {}
-        novel_seq['GENERATED_SEQUENCE'] = generated_dna_sequence
+        novel_seq["GENERATED_SEQUENCE"] = generated_dna_sequence
         return novel_seq
 
-def sequence_to_FASTA (novel_seq: dict, FASTA_OUTPUT: str):
+
+def sequence_to_FASTA(novel_seq: dict, FASTA_OUTPUT: str):
     """
     Convert novel generated DNA sequence to FASTA format
     """
     fasta_header: str = list(novel_seq.keys())[0]
-    with open(f'{FASTA_OUTPUT}.fa', 'w') as FASTA:
-        FASTA.write(f'>{fasta_header}\n')
+    with open(f"{FASTA_OUTPUT}.fa", "w") as FASTA:
+        FASTA.write(f">{fasta_header}\n")
         FASTA.write(novel_seq[fasta_header])
 
 
--- assignment1/nucleotide_count.py
+++ assignment1/nucleotide_count.py
@@ -5,78 +5,118 @@ from collections import Counter
 FILE = argv[1]
 PRINTOUT = argv[2]
 
+
 def FASTA_IO(FILE) -> dict:
-    """ Parse in valid FASTA file & return dictionary containing header as key and str of sequence as value """
+    """Parse in valid FASTA file & return dictionary containing header as key and str of sequence as value"""
     # Valid FASTA Extensions
-    FASTA_FILE_EXTENSIONS: tuple = ('fasta', 'fas', 'fa', 'fna', 'ffn','faa','mpfa','frn')
+    FASTA_FILE_EXTENSIONS: tuple = (
+        "fasta",
+        "fas",
+        "fa",
+        "fna",
+        "ffn",
+        "faa",
+        "mpfa",
+        "frn",
+    )
     FILENAME: str = os.path.basename(FILE)
     # Check to see if provided File argument is a valid FASTA file
     if FILENAME.endswith(FASTA_FILE_EXTENSIONS):
         # Create list of lines from FASTA file & strip whitespace
         SEQUENCE: list = [line.strip() for line in open(FILE).readlines()]
         fasta: dict = {}
-        fasta[SEQUENCE[0].replace('>', '')] = "".join(SEQUENCE[1:])
+        fasta[SEQUENCE[0].replace(">", "")] = "".join(SEQUENCE[1:])
         return fasta
     else:
-        raise Exception('Please provide a valid FASTA file')
+        raise Exception("Please provide a valid FASTA file")
+
 
 def count_nucleotides(fasta: dict) -> dict:
-    """ Counts nucleotides & returns a dict of counts """
-    base_count: dict = {'A':0, 'T':0, 'G': 0, 'C': 0}
+    """Counts nucleotides & returns a dict of counts"""
+    base_count: dict = {"A": 0, "T": 0, "G": 0, "C": 0}
     fasta_header: str = list(fasta.keys())[0]
     # Convert sequence str from fasta dict into a list of individual characters
     sequence: list = [*fasta[fasta_header]]
     for nucleotide in sequence:
         match nucleotide:
-            case 'A' | 'a':
-                base_count['A'] += 1
-            case 'T' | 't':
-                base_count['T'] += 1
-            case 'G' | 'g':
-                base_count['G'] += 1
-            case 'C' | 'c':
-                base_count['C'] += 1
+            case "A" | "a":
+                base_count["A"] += 1
+            case "T" | "t":
+                base_count["T"] += 1
+            case "G" | "g":
+                base_count["G"] += 1
+            case "C" | "c":
+                base_count["C"] += 1
             case _:
                 continue
     return dict(sorted(base_count.items()))
 
+
 def count_dinucleotides(fasta: dict) -> dict:
-    """ Counts dinucleotides & returns a dict of counts """
+    """Counts dinucleotides & returns a dict of counts"""
     dinucleotides: list = []
     fasta_header: str = list(fasta.keys())[0]
     # Convert sequence str from fasta dict into a list of individual characters
     sequence: list = [*fasta[fasta_header]]
     # Iterate through sequence & take slice of sequence list (2 nucleotides) and join together as a string and append to list
     for nucleotide_index in range(len(sequence) - 1):
-        dinucleotide_slice: str = "".join(sequence[nucleotide_index:(nucleotide_index + 2)])
+        dinucleotide_slice: str = "".join(
+            sequence[nucleotide_index : (nucleotide_index + 2)]
+        )
         dinucleotides.append(dinucleotide_slice)
     # Utilize Counter subclass to tally up the dinucleotides
     dinucleotide_count: dict = dict(Counter(dinucleotides))
     # Filter out non-canonical nucleotides from dinucleotide counts by iterating over list of dinucleotide keys ('AT', 'GC', etc) and removing them from dinucleotide count dictionary
-    non_canonical_nucleotides: list = ['Y', 'N', 'W', 'R', 'B', 'H', 'V', 'S', 'K', 'D', 'M', 'U']
+    non_canonical_nucleotides: list = [
+        "Y",
+        "N",
+        "W",
+        "R",
+        "B",
+        "H",
+        "V",
+        "S",
+        "K",
+        "D",
+        "M",
+        "U",
+    ]
     for dinucleotide in list(dinucleotide_count.keys()):
         if any(nucleotide in dinucleotide for nucleotide in non_canonical_nucleotides):
             del dinucleotide_count[dinucleotide]
     return dict(sorted(dinucleotide_count.items()))
+
 
 def calculate_nucleotide_frequency(base_count: dict) -> dict:
     """Calculate nucleotide frequencies from base counts"""
     total_valid_basecount: int = sum(base_count.values())
     nucleotide_frequencies: dict = {}
     # Calculate nucleotide frequency and format to 3 decimal places
-    nucleotide_frequencies['A']: str = "{:.3f}".format(base_count['A'] / total_valid_basecount)
-    nucleotide_frequencies['T']: str = "{:.3f}".format(base_count['T'] / total_valid_basecount)
-    nucleotide_frequencies['G']: str = "{:.3f}".format(base_count['G'] / total_valid_basecount)
-    nucleotide_frequencies['C']: str = "{:.3f}".format(base_count['C'] / total_valid_basecount)
+    nucleotide_frequencies["A"]: str = "{:.3f}".format(
+        base_count["A"] / total_valid_basecount
+    )
+    nucleotide_frequencies["T"]: str = "{:.3f}".format(
+        base_count["T"] / total_valid_basecount
+    )
+    nucleotide_frequencies["G"]: str = "{:.3f}".format(
+        base_count["G"] / total_valid_basecount
+    )
+    nucleotide_frequencies["C"]: str = "{:.3f}".format(
+        base_count["C"] / total_valid_basecount
+    )
     return dict(sorted(nucleotide_frequencies.items()))
 
+
 def calculate_dinucleotide_frequency(dinucleotide_count: dict) -> dict:
-    """ Calculate dinucleotide frequencies"""
+    """Calculate dinucleotide frequencies"""
     dinucleotide_frequency: dict = {}
     total_valid_dinucleotide_count: int = sum(dinucleotide_count.values())
     for dinucleotide, count in dinucleotide_count.items():
-        dinucleotide_frequency[dinucleotide]: str = "{:.3f}".format(count / total_valid_dinucleotide_count)
+        dinucleotide_frequency[dinucleotide]: str = "{:.3f}".format(
+            count / total_valid_dinucleotide_count
+        )
     return dict(sorted(dinucleotide_frequency.items()))
+
 
 fasta = FASTA_IO(FILE)
 nucleotide_count = count_nucleotides(fasta)
@@ -84,18 +124,18 @@ nucleotide_frequency = calculate_nucleotide_frequency(nucleotide_count)
 dinucleotide_count = count_dinucleotides(fasta)
 dinucleotide_frequency = calculate_dinucleotide_frequency(dinucleotide_count)
 
-if PRINTOUT == 'count':
+if PRINTOUT == "count":
     for nucleotide, count in nucleotide_count.items():
-        print(f'{nucleotide}:{count}')
-elif PRINTOUT == 'frequency':
+        print(f"{nucleotide}:{count}")
+elif PRINTOUT == "frequency":
     for nucleotide, frequency in nucleotide_frequency.items():
-        print(f'{nucleotide}:{frequency}')
-elif PRINTOUT == 'dicount':
+        print(f"{nucleotide}:{frequency}")
+elif PRINTOUT == "dicount":
     for dinucleotide, count in dinucleotide_count.items():
-        print(f'{dinucleotide}:{count}')
-elif PRINTOUT == 'difrequency':
-    print('Dinucleotide Frequencies')
+        print(f"{dinucleotide}:{count}")
+elif PRINTOUT == "difrequency":
+    print("Dinucleotide Frequencies")
     for dinucleotide, frequency in dinucleotide_frequency.items():
-        print(f'{dinucleotide}:{frequency}')
+        print(f"{dinucleotide}:{frequency}")
 else:
-    raise Exception('Provide valid printout option')
+    raise Exception("Provide valid printout option")
