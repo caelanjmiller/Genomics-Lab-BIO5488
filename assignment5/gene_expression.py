b102a08919ab54d7d9b4fc64871fe51074f09107
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-02-27 13:10:07 -0600

    Created a fxn to add sample name to count data to track with gene names

--- assignment5/gene_expression.py
+++ assignment5/gene_expression.py
@@ -2,10 +2,8 @@
 
 from sys import argv, exit
 from pathlib import Path
-import matplotlib.pyplot as plt
-from statistics import mean
+from collections import defaultdict
 import csv
-import os 
 
 """"
 Python script to 
@@ -13,9 +11,10 @@ Python script to
 
 EXPRESSION_DATA = Path(argv[1])
 
+
 def RNA_SEQ_IO(FILE: Path) -> dict:
     """
-    Parse in RNA Seq Data and return a dict with gene 
+    Parse in RNA Seq Data and return a dict with gene
     as the key and sample counts as the value
     """
     with open(FILE, "r") as rna_seq:
@@ -35,19 +34,36 @@ def RNA_SEQ_IO(FILE: Path) -> dict:
             rna_seq_data: dict = {}
             for gene_count_data in file_contents:
                 gene_name: str = gene_count_data[0]
-                rna_seq_data[gene_name] = [*map(int, gene_count_data[1:])]            
+                rna_seq_data[gene_name] = [*map(int, gene_count_data[1:])]
             return dict(sorted(rna_seq_data.items()))
 
-def create_transposed_sample_dict (rna_seq_data: dict) -> dict:
+
+def create_transposed_sample_dict(rna_seq_data: dict) -> dict:
     """Create skeleton dict for use in transposing data"""
     # Grab an arbitrary dict element and calculate the number of unique Before & After Sample number
     number_samples: int = int((len(next(iter(rna_seq_data.values()))) / 2))
     transposed_skeleton_data: dict = {}
     for sample_number in range(1, number_samples + 1):
-        transposed_skeleton_data[f"Before_{sample_number}"] = []
+        transposed_skeleton_data[f"Before_{sample_number}"] = {}
     for sample_number in range(1, number_samples + 1):
-        transposed_skeleton_data[f"After_{sample_number}"] = []
+        transposed_skeleton_data[f"After_{sample_number}"] = {}
     return transposed_skeleton_data
+
+
+def add_sample_number(rna_seq_data: dict) -> dict:
+    """Add Sample Name (e.g. Before_1 ) to list of gene count data"""
+    # Initialize defaultdict to create nested dictionary - [Gene][Sample_Name] = [Count_Data]
+    annotated_rna_seq_data: defaultdict = defaultdict(dict)
+    number_samples: int = int((len(next(iter(rna_seq_data.values()))) / 2))
+    for gene_name, count_data in rna_seq_data.items():
+        for index, sample_count in enumerate(count_data):
+            if index <= number_samples:
+                sample_name: str = f"Before_{index}"
+                annotated_rna_seq_data[gene_name][sample_name] = sample_count
+            else:
+                sample_name: str = f"After_{index - number_samples}"
+                annotated_rna_seq_data[gene_name][sample_name] = sample_count
+    return dict(sorted(annotated_rna_seq_data.items()))
 
 
 def transpose_data(rna_seq_data: dict, transposed_skeleton_data: dict) -> dict:
@@ -62,9 +78,11 @@ def transpose_data(rna_seq_data: dict, transposed_skeleton_data: dict) -> dict:
             pass
     return sample_names
 
+
 def filter_zero_gene_expression(transposed_data: dict) -> dict:
     """Filter out genes with 0 count data within a RNA Seq dataset"""
     pass
+
 
 def counts_per_million() -> dict:
     """"""
@@ -75,11 +93,11 @@ def fishers_linear_discriminant():
     """"""
     pass
 
+
 rna_seq_data: dict = RNA_SEQ_IO(EXPRESSION_DATA)
+annotated_rna_seq_data: dict = add_sample_number(rna_seq_data)
 transposed_skeleton_data: dict = create_transposed_sample_dict(rna_seq_data)
-transposed_rna_seq_data = transpose_data(rna_seq_data, transposed_skeleton_data)
-
-
+# transposed_rna_seq_data = transpose_data(rna_seq_data, transposed_skeleton_data)
 
 if len(argv) != 2:
     print(__doc__)
