cf63b1fe09134549b799d94716796a4fe7c51214
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-03-21 21:36:33 -0500

    Tweaked VCF_IO parser fxn

--- assignment8/gv_count.py
+++ assignment8/gv_count.py
@@ -15,11 +15,11 @@ if len(argv) != 3:
     exit(1)
 
 
-def SV_IO(SV_VCF) -> pd.DataFrame:
-    """Parse SV VCF & return dataframe of SV data"""
+def VCF_IO(VCF) -> pd.DataFrame:
+    """Parse VCF & return dataframe of SV data"""
     # Parse raw file contents into list
     raw_file_contents: list = [
-        line.strip().split("\t") for line in open(SV_VCF).readlines()
+        line.strip().split("\t") for line in open(VCF).readlines()
     ]
     # Get index of header
     header_start_index: int = [
@@ -30,13 +30,9 @@ def SV_IO(SV_VCF) -> pd.DataFrame:
     # Filter (skip) rows of data in VCF & create list of data
     filtered_file_contents: list = raw_file_contents[header_start_index + 1 :]
     # Initialize a dataframe of the data and return it
-    RAW_SV_DF = pd.DataFrame(filtered_file_contents, columns=header_contents)
-    return RAW_SV_DF
+    RAW_VCF_DF = pd.DataFrame(filtered_file_contents, columns=header_contents)
+    return RAW_VCF_DF
 
 
-def SNV_IO(SNV_VCF) -> list:
-    """Parse SNV VCF & return dataframe of SNV data"""
-    pass
-
-
-SV_RAW_DF: pd.DataFrame = SV_IO(SV_FILE)
+SV_RAW_DF: pd.DataFrame = VCF_IO(SV_FILE)
+SNV_RAW_DF: pd.DataFrame = VCF_IO(SNV_FILE)
