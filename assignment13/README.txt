b20f1eb930dff31a2b161e96796c84b63645bb38
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-04-30 21:43:18 -0500

    Calculated wobble position conservation - last ditch effort

--- assignment13/README.txt
+++ assignment13/README.txt
@@ -1,12 +1,18 @@
 Usage:
 {How to run neutral_rate.py}
+neutral_rate.py PRE1.aln PRE1.fa
 -
 Question 1:
 {Fraction of wobble positions conserved}
+0.4623
 -
 Question 2
 {Cutoff for 10bp sequence conserved}
+7 bp
 {Explanation}
+I utilized the percent point function (ppf) for the binomial class, where I defined .4623 as the probability
+of neutral conservation, 10 bp as the n, and .95 as 
+num_bp = int(scipy.stats.binom.ppf(.95, 10, .4623))
 -
 Question 3:
 {Number of regions}
@@ -18,7 +24,3 @@ Question 4:
 Question 5:
 {Do binding sites point to proteasome assembly}
 -
-Comments:
-{Things that went wrong or you can not figure out}
--
-
--- assignment13/neutral_rate.py
+++ assignment13/neutral_rate.py
@@ -3,13 +3,17 @@
 """
 Python script to calculate the fraction of wobble positions that
 are conserved across all species in a user provided CLUSTAL W alignment file
-Usage: python3 neutral_rate.py <clustalw alignment file>
+Usage: python3 neutral_rate.py <clustalw alignment file> <FASTA file>
 """
 
-import os
-from itertools import islice
+import scipy
+import itertools
 from sys import argv
 from pathlib import Path
+from Bio import SeqIO, AlignIO
+from textwrap import wrap
+
+import scipy.stats
 
 CLUSTALW_ALN: Path = Path(argv[1])
 FASTA_SEQ: Path = Path(argv[2])
@@ -17,43 +21,10 @@ FASTA_SEQ: Path = Path(argv[2])
 
 def FASTA_IO(FILE) -> dict:
     """Parse in valid FASTA file & return dictionary containing header as key and str of sequence as value"""
-    # Valid FASTA Extensions
-    FASTA_FILE_EXTENSIONS: tuple = (
-        "fasta",
-        "fas",
-        "fa",
-        "fna",
-        "ffn",
-        "faa",
-        "mpfa",
-        "frn",
-    )
-    FILENAME: str = os.path.basename(FILE)
-    # Check to see if provided File argument is a valid FASTA file
-    if FILENAME.endswith(FASTA_FILE_EXTENSIONS):
-        # Create list of lines from FASTA file & strip whitespace
-        SEQUENCES: list = [line.strip() for line in open(FILE).readlines()]
-        fasta: dict = {}
-        # Capture indices at which a FASTA header is seen
-        fasta_header_indices: list = [
-            index for index, line in enumerate(SEQUENCES) if line.startswith(">")
-        ]
-        # Capture FASTA Header & Sequence as a list of lists
-        sequence_information_indices: list = [
-            tuple((fasta_header_indices[index], fasta_header_indices[index + 1]))
-            for index in range(len(fasta_header_indices) - 1)
-        ]
-        captured_fasta_information: list = [
-            list(islice(SEQUENCES, index, sequences))
-            for index, sequences in sequence_information_indices
-        ]
-        for FASTA_ENTRY in captured_fasta_information:
-            fasta_header = FASTA_ENTRY[0].replace(">", "")
-            fasta_sequence = "".join(FASTA_ENTRY[1:])
-            fasta[fasta_header] = fasta_sequence
-        return fasta
-    else:
-        raise Exception("Please provide a valid FASTA file")
+    fasta_sequences: dict = {}
+    for sequence_record in SeqIO.parse(FILE, "fasta"):
+        fasta_sequences[sequence_record.id] = str(sequence_record.seq)
+    return fasta_sequences
 
 
 def extract_CDS_sequence(fasta_sequences: dict) -> dict:
@@ -70,7 +41,56 @@ def extract_CDS_sequence(fasta_sequences: dict) -> dict:
     return cds_sequences
 
 
+def alignment_IO(alignment_file: Path):
+    """Parse CLUSTAL alignment file & return an BioPython alignment object"""
+    alignment = AlignIO.read(alignment_file, "clustal")
+    return alignment
+
+
+def nucleotide_split_codons(cds_sequences: dict) -> dict:
+    """Split CDS Sequences into putative codons (sets of 3)"""
+    codon_sequences: dict = {}
+    for header, sequence in cds_sequences.items():
+        codons = wrap(sequence, 3)
+        codon_sequences[header] = codons
+    return codon_sequences
+
+
+def calculate_wobble_conservation(codon_sequences: dict) -> float:
+    """Calculate fraction of conserved wobble positions across CDS sequences"""
+    third_nucleotide_str_dict: dict = {}
+    for header, codon_sequence in codon_sequences.items():
+        third_string: list = []
+        for codon in codon_sequence:
+            third_string.append(codon[2])
+        third_nucleotide_str_dict[header] = third_string
+    total_wobble_positions: int = int(
+        (len(next(iter(third_nucleotide_str_dict.values()))))
+    )
+    sequence_one: list = third_nucleotide_str_dict["Scer"]
+    sequence_two: list = third_nucleotide_str_dict["Skud"]
+    sequence_three: list = third_nucleotide_str_dict["Smik"]
+    sequence_four: list = third_nucleotide_str_dict["Sbay"]
+    conserved_positions: int = 0
+    for i, j, x, z in itertools.zip_longest(
+        sequence_one, sequence_two, sequence_three, sequence_four
+    ):
+        if i == j and i == x and i == z:
+            conserved_positions += 1
+        else:
+            continue
+    wobble_conservation: float = round(
+        float(conserved_positions / total_wobble_positions), 4
+    )
+    return wobble_conservation
+
+
+CLUSTALW_alignment = alignment_IO(CLUSTALW_ALN)
 fasta_sequences: dict = FASTA_IO(FASTA_SEQ)
 cds_sequences: dict = extract_CDS_sequence(fasta_sequences)
-for name, sequence in cds_sequences.items():
-    print(f"{name}: {len(sequence)}")
+codon_sequences: dict = nucleotide_split_codons(cds_sequences)
+wobble_conservation: float = calculate_wobble_conservation(codon_sequences)
+print(f"Conserved wobble positions: {wobble_conservation}")
+print(
+    f"Number of bp for a 10bp sequence to be more conserved than expected: {int(scipy.stats.binom.ppf(.95, 10, wobble_conservation))}"
+)
